88cd0a19779442303832e8d55ec2c87e1b955286
gabi <141293347+gabrieeela@users.noreply.github.com>
2024-04-24 17:42:07 -0300

    Add files via upload

---
+++ fetch/index.js
@@ -0,0 +1,20 @@
+//promise
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((error) => {
+    console.log(error);
+  });
+
+//await
+async function loadData() {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  loadData();  
